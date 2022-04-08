b099fb86637f8c80f96e098602673dfd6896e27f
QuocAnh <anhit.luxuryfi@gmail.com>
2022-04-08 10:50:32 +0700

    Update unit test

--- tests/services/emailService.test.js
+++ tests/services/emailService.test.js
@@ -14,13 +14,16 @@ const {
   IDEA_STATUS
 } = require('../../src/configs/ms-constants');
 const emailService = require('../../src/services/emailService.js');
+
 const customMessages = require('../../src/configs/customMessages');
 const fs = require('fs');
 const {
   email
 } = require('../../src/configs/config');
+const { EMAIL_SLUGS } = require('../../src/configs/emailSlugs');
 
 require('mysql2/node_modules/iconv-lite').encodingExists('foo');
+const nodemailer = require('nodemailer');
 
 jest.useFakeTimers()
 const mockResponse = () => {
@@ -32,6 +35,66 @@ const mockResponse = () => {
   return res;
 };
 
+
 describe('Test email service', () => {
-  
+  describe('Test account created email send', () => {
+    it('it should send', async () => {
+      jest.spyOn(nodemailer, 'createTransport').mockResolvedValueOnce({
+        sendMail: () => ({
+          error: false
+        }),
+      });
+
+      await emailService.sendEmail({
+        email_slug: EMAIL_SLUGS.ACCOUNT_CREATED,
+        username: 'Test',
+        passsword: 'Test',
+      });
+      expect(true).toBe(true);
+    })
+
+    it('it should send', async () => {
+      jest.spyOn(nodemailer, 'createTransport').mockResolvedValueOnce({
+        sendMail: () => ({
+          error: false
+        }),
+      });
+
+      await emailService.sendEmail({
+        email_slug: EMAIL_SLUGS.PASSWORD_RESET,
+        username: 'Test',
+        passsword: 'Test',
+      });
+      expect(true).toBe(true);
+    })
+
+    it('it should send', async () => {
+      jest.spyOn(nodemailer, 'createTransport').mockResolvedValueOnce({
+        sendMail: () => ({
+          error: false
+        }),
+      });
+
+      await emailService.sendEmail({
+        email_slug: EMAIL_SLUGS.IDEA_CREATED,
+        username: 'Test',
+        passsword: 'Test',
+      });
+      expect(true).toBe(true);
+    })
+
+    it('it should send', async () => {
+      jest.spyOn(nodemailer, 'createTransport').mockResolvedValueOnce({
+        sendMail: () => ({
+          error: false
+        }),
+      });
+      await emailService.sendEmail({
+        email_slug: EMAIL_SLUGS.IDEA_COMMENT,
+        username: 'Test',
+        passsword: 'Test',
+      });
+      expect(true).toBe(true);
+    })
+  })
 })
--- tests/utils/emailTemplate.test.js
+++ tests/utils/emailTemplate.test.js
@@ -3,10 +3,13 @@ const emailTemplate = require('../../src/utils/emailTemplate');
 
 test('Test emailtemplate', () => {
   const slug = 'account_created'
-  emailTemplate.accountCreatedTemplate('name', 'username', '123123');
-  emailTemplate.ideaCommentTemplate('name', 'username', '123123',1,1);
-  emailTemplate.ideaCreatedTemplate('name', 'username', '123123','123123','test');
-  emailTemplate.resetPasswordTemplate('name', 'username', '123123','13123');
+  const createdAccount = emailTemplate.accountCreatedTemplate('name', 'username', '123123');
+  const ideaComment = emailTemplate.ideaCommentTemplate('name', 'username', '123123',1,1);
+  const ideaCreated = emailTemplate.ideaCreatedTemplate('name', 'username', '123123','123123','test');
+  const resetPassword = emailTemplate.resetPasswordTemplate('name', 'username', '123123','13123');
 
-  expect(0).toBe(0);
+  expect(createdAccount).not.toBe(null);
+  expect(ideaComment).not.toBe(null);
+  expect(ideaCreated).not.toBe(null);
+  expect(resetPassword).not.toBe(null);
 })
